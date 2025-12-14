5e701c5e0a3769d77c00c1cc7eb61ac21e182d23
paulboul1013 <paulboul1013@gmail.com>
2025-12-14 12:35:49 +0800

    move kernel & boot code to the kernel/ and boot/

--- kernel.c
+++
@@ -1,8 +0,0 @@
-//this is force us to create a kernel entry routinue which does not point to byte 0x0 in our kernel，but to an actual label which we know that launches it，the case is `main()`
-void dummy_test_entrypoint(){}
-
-
-void main(){
-    char *video_memory = (char*)0xb8000;
-    *video_memory = 'X';
-}
--- kernel_entry.asm
+++
@@ -1,4 +0,0 @@
-[bits 32]
-[extern main] ;define calling point,must have same name as kernel.c main function
-call main;
-jmp $
