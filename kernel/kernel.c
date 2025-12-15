1a924df43a266c473a3d14fe18acb20fb04e940b
paulboul1013 <paulboul1013@gmail.com>
2025-12-15 16:29:06 +0800

    write get cursor position & show vga character

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,27 @@
-//this is force us to create a kernel entry routinue which does not point to byte 0x0 in our kernel，but to an actual label which we know that launches it，the case is `main()`
-void dummy_test_entrypoint(){}
+#include "../drivers/ports.h"
 
 
 void main(){
-    char *video_memory = (char*)0xb8000;
-    *video_memory = 'X';
+
+
+    //screen cursor position: ask vga control register (0x3d4) for bytes
+    // 14 is high byte, 15 is low byte
+    port_byte_out(0x3d4, 14);//request the high byte
+
+    int position =port_byte_in(0x3d5);//data is returned in vga register(0x3d5)
+    position = position << 8; //high byte
+
+    port_byte_out(0x3d4, 15);//request the low byte
+    position+=port_byte_in(0x3d5);
+
+
+    //vga cells consist of the character and color attributes
+    int offset_from_vga=position*2;
+
+
+    char *vga =(char*)0xb8000;
+    vga[offset_from_vga]='X';
+    vga[offset_from_vga+1]=0x0f;//white text on black
+
+
 }
