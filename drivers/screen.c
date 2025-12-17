a84a223853cc0ed33f716773baec38a6491157fc
paulboul1013 <paulboul1013@gmail.com>
2025-12-17 22:10:14 +0800

    Add the print message function & get cursor offset

---
+++ drivers/screen.c
@@ -0,0 +1,131 @@
+#include "screen.h"
+#include "ports.h"
+
+//private functions
+int get_cursor_offset();
+void set_cursor_offset(int offset);
+int print_char(char c,int col,int row,char attr);
+int get_offset(int col,int row);
+int get_offset_row(int offset);
+int get_offset_col(int offset);
+
+//public screen API
+void kprint_at(char *message,int col,int row){
+    //set cursor if col/row are negative
+    int offset;
+    if (col>=0 && row>=0){
+        offset=get_offset(col,row);
+    }else{
+        offset=get_cursor_offset();
+        row=get_offset_row(offset);
+        col=get_offset_col(offset);
+    }
+
+    //loop through message and print it
+    int i=0;
+    while (message[i]!=0){
+        offset=print_char(message[i++],col,row,WHITE_ON_BLACK);
+
+        //compute row/col for next iteration
+        row=get_offset_row(offset);
+        col=get_offset_col(offset);
+
+    }
+
+}
+
+void kprint(char *message){
+    kprint_at(message,-1,-1);
+}
+
+
+//private kernel functions
+
+
+//innertmost print function for kernel,directly accesses the video memory
+
+//if col and row are negative，will print at current cursor position
+//it attr is zero it will use white on black as default
+//returns the offset of the next character
+//sets the video cursor to the returned offset
+
+int print_char(char c,int col,int row,char attr){
+    unsigned char *vidmem=(unsigned char *)VIDEO_ADDRESS;
+    if (!attr) {
+        attr=WHITE_ON_BLACK;
+    }
+
+    //error control: print a red E  if the coords aren't right
+    if (col>=MAX_COLS || row>=MAX_ROWS) {
+        vidmem[2*(MAX_COLS)*(MAX_ROWS)-2]='E';
+        vidmem[2*(MAX_COLS)*(MAX_ROWS)-1]=RED_ON_WHITE;
+        return get_offset(col,row);
+    }
+
+    int offset;
+    if (col>=0 && row>=0) {
+        offset=get_offset(col,row);
+    }else{ 
+        //if col and row are negative, use cursor offset
+        offset=get_cursor_offset();
+    }
+
+    if (c=='\n'){ //handle newline
+        row=get_offset_row(offset);
+        offset=get_offset(0,row+1);
+    }else{ //print the character
+        vidmem[offset]=c;
+        vidmem[offset+1]=attr;
+        offset+=2;
+    }
+
+    set_cursor_offset(offset);
+    return offset;
+   
+}
+
+int get_cursor_offset(){
+    //use the VGA ports to get the current cursor position
+    //1. ask for high byte of the cursor offset (data 14)
+    //2. ask for low byte of the cursor offset (data 15)
+
+    port_byte_out(REG_SCREEN_CTRL,14);
+    int offset=port_byte_in(REG_SCREEN_DATA)<<8; 
+    port_byte_out(REG_SCREEN_CTRL,15);
+    offset+=port_byte_in(REG_SCREEN_DATA);
+    return offset*2; //position *size of character cell
+
+}
+
+void set_cursor_offset(int offset){
+    //like get_cursor_offset() ,but instead of reading write data
+    offset/=2;
+    port_byte_out(REG_SCREEN_CTRL,14);
+    port_byte_out(REG_SCREEN_DATA,(unsigned char)(offset>>8));
+    port_byte_out(REG_SCREEN_CTRL,15);
+    port_byte_out(REG_SCREEN_DATA,(unsigned char)(offset & 0xff));
+}
+
+void clear_screen() {
+    int screen_size=MAX_COLS*MAX_ROWS;
+    int i;
+    char *screen=(char*)VIDEO_ADDRESS;
+
+    for(i=0;i<screen_size;i++) {
+        screen[i*2]=' ';
+        screen[i*2+1]=WHITE_ON_BLACK;
+    }
+    set_cursor_offset(get_offset(0,0));
+}
+
+int get_offset(int col,int row){
+    return 2*(row*MAX_COLS+col);
+}
+
+int get_offset_row(int offset){
+    return offset/(2*MAX_COLS);
+}
+
+int get_offset_col(int offset){
+    return (offset-(get_offset_row(offset)*2*MAX_COLS))/2;
+}
