09eb8a6e30d7ab8a22c91e592f0bb53e9dac8945
paulboul1013 <paulboul1013@gmail.com>
2025-12-18 07:30:10 +0800

    Add scroll test

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,13 +1,23 @@
 #include "../drivers/screen.h"
-
+#include "util.h"
 
 void main(){
 
     clear_screen();
-    kprint_at("X", 1, 6);
-    kprint_at("This text spans multiple lines",75,10);
-    kprint_at("There is a line\nbreak", 0, 20);
-    kprint("There is a line \n break");
-    kprint_at("What happens when we run out of space?", 45, 24);
+   
+
+    //fill the screen with characters
+
+    int i=0;
+    for(i=0;i<24;i++){
+        char str[255];
+        int_to_ascii(i,str);
+        kprint_at(str,0,i);
+    }
+
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
+    kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
+    kprint("\nanother line");
+
 
 }
