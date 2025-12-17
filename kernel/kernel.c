461ad64c1249df2412e01038739fce309e3db7ac
paulboul1013 <paulboul1013@gmail.com>
2025-12-17 22:12:27 +0800

    Add the strings on the VGA screen test

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,27 +1,13 @@
-#include "../drivers/ports.h"
+#include "../drivers/screen.h"
 
 
 void main(){
 
-
-    //screen cursor position: ask vga control register (0x3d4) for bytes
-    // 14 is high byte, 15 is low byte
-    port_byte_out(0x3d4, 14);//request the high byte
-
-    int position =port_byte_in(0x3d5);//data is returned in vga register(0x3d5)
-    position = position << 8; //high byte
-
-    port_byte_out(0x3d4, 15);//request the low byte
-    position+=port_byte_in(0x3d5);
-
-
-    //vga cells consist of the character and color attributes
-    int offset_from_vga=position*2;
-
-
-    char *vga =(char*)0xb8000;
-    vga[offset_from_vga]='X';
-    vga[offset_from_vga+1]=0x0f;//white text on black
-
+    clear_screen();
+    kprint_at("X", 1, 6);
+    kprint_at("This text spans multiple lines",75,10);
+    kprint_at("There is a line\nbreak", 0, 20);
+    kprint("There is a line \n break");
+    kprint_at("What happens when we run out of space?", 45, 24);
 
 }
