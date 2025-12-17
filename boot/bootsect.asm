4d3f433ea3ea8f493b154b6b02e1e25633dde131
paulboul1013 <paulboul1013@gmail.com>
2025-12-17 22:18:47 +0800

    change dh to 16 to fit the kernel size

--- boot/bootsect.asm
+++ boot/bootsect.asm
@@ -28,7 +28,7 @@ load_kernel:
     call print_nl
 
     mov bx,KERNEL_OFFSET ; read from disk and store in 0x1000
-    mov dh ,2
+    mov dh ,16 ; dh is for read 16 sectors。our future kernel will be larger，make it bigger
     mov dl,[BOOT_DRIVE]
     call disk_load
     ret
