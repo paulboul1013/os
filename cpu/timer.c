0f9991fb1ed9102c2fb10fd80a0404bef5d739d7
paulboul1013 <paulboul1013@gmail.com>
2025-12-23 10:15:41 +0800

    Add timer delcare & implementation

---
+++ cpu/timer.c
@@ -0,0 +1,32 @@
+#include "timer.h"
+#include "../drivers/screen.h"
+#include "../kernel/util.h"
+#include "isr.h"
+
+
+u32 tick=0;
+
+static void timer_callback(registers_tregs){
+    tick++;
+    kprint("Tick: ");
+
+    char tick_ascii[256];
+    int_to_ascii(tick,tick_ascii);
+    kprint(tick_ascii);
+    kprint("\n");
+}
+
+void init_timer(u32 freq) {
+    //install the function just wrote
+    register_interrupt_handler(IRQ0,timer_callback);
+
+    //Get the PIT value: hardware clock at 1193180 Hz
+    u32 divisor = 1193180 / freq;
+    u8 low=(u8)(divisor & 0xFF);
+    u8 high=(u8)((divisor>>8) &0xFF);
+    //send the command
+    port_byte_out(0x43,0x36); //command port
+    port_byte_out(0x40,low);
+    port_byte_out(0x40,high);
+}
+
