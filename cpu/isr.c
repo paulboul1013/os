df2c810246884e2dcf7cbcb32e6cadec3d9c02c1
paulboul1013 <paulboul1013@gmail.com>
2025-12-25 10:31:16 +0800

    Add some headers & Add install_irq

--- cpu/isr.c
+++ cpu/isr.c
@@ -1,7 +1,10 @@
 #include "isr.h"
 #include "idt.h"
 #include "../drivers/screen.h"
-#include "../kernel/util.h"
+#include "../drivers/ports.h"
+#include "../lib/string.h"
+#include "timer.h"
+#include "ports.h"
 
 isr_t interrupt_handlers[256];
 
@@ -139,4 +142,14 @@ void irq_handler(registers_t r){
         isr_t handler=interrupt_handlers[r.int_no];
         handler(r);
     }
+}
+
+void irq_install() {
+    //enable interruptions
+    asm volatile("sti");
+
+    //IRQ0: timer
+    init_timer(50);
+    //IRQ1: keyboard
+    init_keyboard();
 }
