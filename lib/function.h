1adcd7a401ecc3ad72d31b0d5c9b4b100a0d6614
paulboul1013 <paulboul1013@gmail.com>
2025-12-25 10:41:46 +0800

    Add UNUSED Marco

---
+++ lib/function.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+//sometimes want to keep parameters to a function for later use
+// and avoid the unused parameter compiler warning
+#define UNUSED(x) (void)(x)
+
+#endif
