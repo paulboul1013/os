d0505c7542145e93079cfd6d76a836eef4e6d543
paulboul1013 <paulboul1013@gmail.com>
2025-12-26 17:03:17 +0800

    build the user input

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,15 +1,23 @@
 #include "../cpu/isr.h"
-#include "../cpu/timer.h"
-#include "../drivers/keyboard.h"
+#include "../drivers/screen.h"
+#include "kernel.h"
+#include "../lib/string.h"
 
 void main(){
     isr_install();
+    irq_install();
    
-    asm volatile("sti");
-    init_timer(50);
+    kprint("Type something, it will go through the kernel\n"
+        "Type END to halt the CPU\n>");
+}
 
-
-    init_keyboard();
-   
+void user_input(char *input){
+    if (strcmp(input,"END")==0){
+        kprint("Stopping the CPU\n");
+        asm volatile("hlt");
+    }
+    kprint("You typed: ");
+    kprint(input);
+    kprint("\n>");
 
 }
