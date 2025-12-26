5a5f8d2d03a23c047f024b8fcd9d387fa66b55f6
paulboul1013 <paulboul1013@gmail.com>
2025-12-26 17:10:49 +0800

    add global variable to track input offset to limit backsapce go back

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -10,6 +10,7 @@
 #define ENTER 0x1C
 
 static  char key_buffer[256];
+static int input_line_start_offset = 0; // Track the start of current input line
 
 
 #define SC_MAX 57
@@ -20,10 +21,9 @@ const char *sc_name[] = {
         "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl", 
         "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", 
         "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", 
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar"
-};
+        "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
 
-const char *char sc_ascii[] ={
+const char  sc_ascii[] ={
     '?', '?', '1', '2', '3', '4', '5', '6',     
     '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y', 
         'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G', 
@@ -38,12 +38,17 @@ static void keyboard_callback(registers_t regs){
 
     if (scancode > SC_MAX) return;
     if (scancode == BACKSPACE){
-        backspace(key_buffer);
-        kprint_backspace();
+        // Only allow backspace if there are characters in buffer and cursor is past input line start
+        if (strlen(key_buffer) > 0) {
+            backspace(key_buffer);
+            kprint_backspace(input_line_start_offset);
+        }
     }else if (scancode ==ENTER){
         kprint("\n");
-        user_input(ker_buffer);//kernel-controlled function
+        user_input(key_buffer);//kernel-controlled function
         key_buffer[0]='\0';
+        // Update input line start to current cursor position (after prompt)
+        input_line_start_offset = get_cursor_offset();
     } else{
         char letter=sc_ascii[(int)scancode];
         //kprint ony accepts char[]
@@ -59,4 +64,6 @@ static void keyboard_callback(registers_t regs){
 
 void init_keyboard() {
     register_interrupt_handler(IRQ1,keyboard_callback);
+    // Set initial input line start to current cursor position (after prompt)
+    input_line_start_offset = get_cursor_offset();
 }
