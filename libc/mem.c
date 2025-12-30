c731e2774b2e90c1de4d499dd0070c28fd1fc98c
paulboul1013 <paulboul1013@gmail.com>
2025-12-30 21:27:21 +0800

    implement the kmalloc()

--- libc/mem.c
+++ libc/mem.c
@@ -13,3 +13,23 @@ void memory_set(u8 *dest,u8 val,u32 len){
         *temp++=val;
     }
 }
+
+//should be cpmputed at link time,but a hardcoded
+//value is fine for now，remember kernel start at 0x10000
+u32 free_mem_addr=0x10000;
+
+//implementation is a pointer to some free memory which keeps growing
+u32 kmalloc(u32 size,int align,u32 *phys_addr){
+    //pages are aligned to 4k , or 0x1000
+    if (align==1 && (free_mem_addr & 0xFFFFF000)){
+        free_mem_addr&=0xFFFFF000;
+        free_mem_addr+=0x1000;
+    }
+
+    //save also the physical address
+    if (phys_addr) *phys_addr=free_mem_addr;
+
+    u32 ret=free_mem_addr;
+    free_mem_addr+=size; //remember to increment the pointer
+    return ret;
+}
