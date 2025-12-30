d09e11127dd836488315f598a7ef76f4e0954ecd
paulboul1013 <paulboul1013@gmail.com>
2025-12-30 21:33:17 +0800

    Add selection for allocate page memory

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,21 +1,36 @@
 #include "../cpu/isr.h"
 #include "../drivers/screen.h"
 #include "kernel.h"
-#include "../lib/string.h"
+#include "../libc/string.h"
+#include "../libc/mem.h"
 
 void main(){
     isr_install();
     irq_install();
    
     kprint("Type something, it will go through the kernel\n"
-        "Type END to halt the CPU\n>");
+        "Type END to halt the CPU or PAGE to request a kmalloc()\n> ");
 }
 
 void user_input(char *input){
     if (strcmp(input,"END")==0){
         kprint("Stopping the CPU\n");
         asm volatile("hlt");
+    }else if (strcmp(input,"PAGE")==0){
+        //get test kmalloc
+        u32 phys_addr;
+        u32 page=kmalloc(1000,1,&phys_addr);
+        char page_str[16]="";
+        hex_to_ascii(page,page_str);
+        char phys_str[16]="";
+        hex_to_ascii(phys_addr,phys_str);
+        kprint("Page: ");
+        kprint(page_str);
+        kprint(", physical address: ");
+        kprint(phys_str);
+        kprint("\n");
     }
+
     kprint("You typed: ");
     kprint(input);
     kprint("\n>");
