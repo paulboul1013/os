8ee2afc82a7e46cce08a14e5a2970164c19e625f
paulboul1013 <paulboul1013@gmail.com>
2025-12-31 20:57:58 +0800

    Add _start label to setting the kernel_main

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -1,4 +1,7 @@
+global _start
 [bits 32]
-[extern main] ;define calling point,must have same name as kernel.c main function
-call main;
-jmp $
+
+_start:
+    [extern kernel_main] ;define calling point,must have same name as kernel.c main function
+    call kernel_main ;call the c function，the linker will know  where it is placed in memory
+    jmp $
