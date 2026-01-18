5bf51a87a5f4204f9df0a45702d2f06b80d85cef
paulboul1013 <paulboul1013@gmail.com>
2026-01-18 12:05:10 +0800

    Add extern call_global_constructors label

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -2,6 +2,9 @@ global _start
 [bits 32]
 
 _start:
+    [extern call_global_constructors]
+    call call_global_constructors
+    
     [extern kernel_main] ;define calling point,must have same name as kernel.c main function
     call kernel_main ;call the c function，the linker will know  where it is placed in memory
     jmp $
